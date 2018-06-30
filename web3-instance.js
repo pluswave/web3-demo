2b47f096ab1bbf7cffcb66cdae48cf83249a033b
Zhang Zengbo <zengbo.zhang@gmail.com>
2018-06-30 18:27:06 +0800

    update instance

--- web3-instance.js
+++ web3-instance.js
@@ -2,10 +2,7 @@
 const Web3 = require('web3');
 
 const web3 = new Web3(new
-   Web3.providers.HttpProvider("https://ropsten.infura.io/my22Z70hMy1BP9MuETwJ")
-   //Web3.providers.HttpProvider("https://parity.ropsten.magicw.net/normal")
-    // Web3.providers.HttpProvider("http://127.0.0.1:8645")
-    // Web3.providers.WebsocketProvider("ws://127.0.0.1:8646")
+   Web3.providers.HttpProvider("https://ropsten.infura.io/PleaseUpdateWithYourKeyInsideInfura")
 );
 
 module.exports = web3;
